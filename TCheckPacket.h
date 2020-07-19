230d19c598cd66a957b483954bbdbbe5f7c5c3f0
kongbiji <heapoverflow@kakao.com>
2020-07-19 14:53:55 +0900

    Initial commit

---
+++ TCheckPacket.h
@@ -0,0 +1,252 @@
+#ifndef TCHECKPACKET_H
+#define TCHECKPACKET_H
+
+#pragma once
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <map>
+#include <vector>
+#include <unistd.h>
+#include "TPacketData.h"
+using namespace std;
+
+map<MAC, Beacon_values> beacon_map;
+map<MAC, Beacon_values>::iterator beacon_iter;
+map<CONV_MAC, Probe_values> probe_map;
+map<CONV_MAC, Probe_values>::iterator probe_iter;
+
+MAC BROADCAST;
+
+void init(){
+    memcpy(BROADCAST.mac,"\xFF\xFF\xFF\xFF\xFF\xFF", sizeof(BROADCAST.mac));
+}
+
+// check Type/Subtpye
+int check_packet_type(const unsigned char * data){
+    radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
+    Dot11 * dot11 = (Dot11 *)malloc(sizeof(Dot11));
+    r_header = (radiotap_header *)data;
+    dot11 = (Dot11 *)(data+r_header->h_len);
+    int RETURN = -1;
+
+    if(dot11->Frame_Control_Field.Type == 0){ // Management frame(type == 0)
+        switch (dot11->Frame_Control_Field.Subtype){
+        case 8:
+            RETURN = BEACON_FRAME;
+            break;
+        case 4:
+            RETURN = PROBE_REQUEST;
+            break;
+        case 5:
+            RETURN = PROBE_RESPONSE;
+        default:
+            break;
+        }
+    }else if(dot11->Frame_Control_Field.Type == 2){
+        if(dot11->Frame_Control_Field.Subtype == 8 ){
+            RETURN = QOS_DATA;
+        }else if(dot11->Frame_Control_Field.Subtype == 12 || dot11->Frame_Control_Field.Subtype == 4){
+            RETURN = QOS_NULL;
+        }
+    }
+    return RETURN;
+}
+
+void print_MAC(uint8_t *addr, char *mac){
+    sprintf(mac,"%02X:%02X:%02X:%02X:%02X:%02X",
+           addr[0],addr[1],addr[2],addr[3],
+            addr[4],addr[5]);
+}
+
+void save_Beacon_info(const unsigned char * data){
+    // for map
+    MAC bssid;
+    Beacon_values val;
+    memset(val.ssid, 0, sizeof(val.ssid));
+    beacon_iter = beacon_map.begin();
+
+    radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
+    Dot11 * dot11 = (Dot11 *)malloc(sizeof(Dot11 *));
+
+    unsigned char * r_header_iter;
+    r_header = (radiotap_header *)data;
+    r_header_iter = (unsigned char *)(data + sizeof(radiotap_header));
+    dot11 = (Dot11 *)(data + r_header->h_len);
+
+    memcpy( bssid.mac, dot11->mac3, sizeof(bssid.mac));
+
+    // save ssid
+    unsigned char * find_ssid = (unsigned char *)(data + r_header->h_len + sizeof(Dot11) + 12);
+    if( find_ssid[0] == 0 ){ // SSID parameter
+        int len = find_ssid[1];
+        if(len != 0){
+            memcpy(val.ssid, find_ssid+2, len);
+        }
+    }
+
+    // radiotap fields check
+    if( r_header->presnt_flags.tsft == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint64_t);
+    }
+    if( r_header->presnt_flags.flags == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.rate == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.channel == 1 ){
+        val.CH = ((int)(r_header_iter[0] | (r_header_iter[1] << 8)) - 2412) / 5 + 1;
+        r_header_iter = r_header_iter + sizeof(uint32_t);
+    }
+    if( r_header->presnt_flags.fhss == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.dbm_antenna_sig == 1 ){
+        val.PWR = ((int)r_header_iter[0]-1)^0xFF;
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+
+    beacon_iter = beacon_map.find(bssid);
+    if(beacon_iter != beacon_map.end()){  // already saved AP, update info
+        memcpy(val.ssid, beacon_iter->second.ssid, sizeof(val.ssid));
+        int num = beacon_iter->second.Beacons;
+        beacon_iter->second = val;
+        beacon_iter->second.Beacons = num + 1;
+    }else{
+        beacon_map.insert(pair<MAC, Beacon_values>(bssid, val));
+    }
+}
+
+void save_Probe_info(const unsigned char * data, int type){
+
+    CONV_MAC key;
+    Probe_values val;
+    memset(val.probe, 0, sizeof(val.probe));
+    probe_iter = probe_map.begin();
+
+    radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
+    Dot11 * dot11 = (Dot11 *)malloc(sizeof(Dot11 *));
+
+    unsigned char * r_header_iter;
+    r_header = (radiotap_header *)data;
+    r_header_iter = (unsigned char *)(data + sizeof(radiotap_header));
+    dot11 = (Dot11 *)(data + r_header->h_len);
+
+    // save probe
+    int type_num;
+    memcpy(key.bssid, dot11->mac3, sizeof(dot11->mac3)); //set bssid
+    switch (type){
+    case 1:  // Probe request
+        type_num = 0;
+        memcpy(key.station, dot11->mac2, sizeof(dot11->mac2)); // set station
+        break;
+    case 2:  // Probe response
+        type_num = 12;
+        memcpy(key.station, dot11->mac1, sizeof(dot11->mac1)); // set station
+        break;
+    default:
+        break;
+    }
+
+    unsigned char * find_probe = (unsigned char *)(data + r_header->h_len + sizeof(Dot11) + type_num);
+    if( (int)find_probe[0] == 0 ){ // SSID parameter
+        int len = (int)find_probe[1];
+        if(len != 0){
+            memcpy(val.probe, find_probe+2, len);
+        }
+    }
+
+    // radiotap fields check
+    if( r_header->presnt_flags.tsft == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint64_t);
+    }
+    if( r_header->presnt_flags.flags == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.rate == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.channel == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint32_t);
+    }
+    if( r_header->presnt_flags.fhss == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.dbm_antenna_sig == 1 ){
+
+        val.PWR = ((int)r_header_iter[0]-1)^0xFF;
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+
+    probe_iter = probe_map.find(key);
+    if(probe_iter != probe_map.end()){  // already saved AP, update info
+        memcpy(val.probe, probe_iter->second.probe, sizeof(val.probe));
+        int num = probe_iter->second.Frames;
+        probe_iter->second = val;
+        probe_iter->second.Frames = num + 1;
+    }else{
+        probe_map.insert(pair<CONV_MAC, Probe_values>(key, val));
+    }
+}
+
+void save_QoS_info(const unsigned char * data, int type){
+    CONV_MAC key;
+    Probe_values val;
+    memset(val.probe, 0, sizeof(val.probe));
+    probe_iter = probe_map.begin();
+
+    radiotap_header * r_header = (radiotap_header *)malloc(sizeof(radiotap_header));
+    Dot11_data * dot11 = (Dot11_data *)malloc(sizeof(Dot11_data *));
+
+    unsigned char * r_header_iter;
+    r_header = (radiotap_header *)data;
+    r_header_iter = (unsigned char *)(data + sizeof(radiotap_header));
+    dot11 = (Dot11_data *)(data + r_header->h_len);
+
+    switch (type){
+    case 3:  // QoS Data
+        memcpy(key.bssid, dot11->mac2, sizeof(dot11->mac2)); // set bssid
+        memcpy(key.station, dot11->mac1, sizeof(dot11->mac1)); // set station
+        /* code */
+        break;
+    case 4:  // QoS NULL
+        memcpy(key.bssid, dot11->mac1, sizeof(dot11->mac1)); //set bssid
+        memcpy(key.station, dot11->mac2, sizeof(dot11->mac2)); // set station
+        break;
+    default:
+        break;
+    }
+    // radiotap fields check
+    if( r_header->presnt_flags.tsft == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint64_t);
+    }
+    if( r_header->presnt_flags.flags == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.rate == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.channel == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint32_t);
+    }
+    if( r_header->presnt_flags.fhss == 1 ){
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+    if( r_header->presnt_flags.dbm_antenna_sig == 1 ){
+        val.PWR = ((int)r_header_iter[0]-1)^0xFF;
+        r_header_iter = r_header_iter + sizeof(uint8_t);
+    }
+
+    probe_iter = probe_map.find(key);
+    if(probe_iter != probe_map.end()){  // already saved AP, update info
+        int num = probe_iter->second.Frames;
+        probe_iter->second = val;
+        probe_iter->second.Frames = num + 1;
+    }else{
+        probe_map.insert(pair<CONV_MAC, Probe_values>(key, val));
+    }
+}
+
+#endif // TCHECKPACKET_H
---
+++ TPacketData.h
@@ -0,0 +1,172 @@
+#ifndef TPACKETDATA_H
+#define TPACKETDATA_H
+
+#pragma once
+#include <stdint.h>
+#include <string.h>
+
+#define BEACON_FRAME 0
+#define PROBE_REQUEST 1
+#define PROBE_RESPONSE 2
+#define QOS_DATA 3
+#define QOS_NULL 4
+
+#pragma pack(push, 1)
+typedef struct present_flags{
+    uint8_t tsft:1;
+    uint8_t flags:1;
+    uint8_t rate:1;
+    uint8_t channel:1;
+    uint8_t fhss:1;
+    uint8_t dbm_antenna_sig:1; // antenna signal awlays 5th bits
+    uint8_t dbm_antenna_noise:1;
+    uint8_t lock_quality:1;
+    uint8_t tx_attenuation:1;
+    uint8_t db_tx_attenuation:1;
+    uint8_t dbm_tx_power:1;
+    uint8_t antenna:1;
+    uint8_t db_antenna_sig:1;
+    uint8_t db_antenna_noise:1;
+    uint8_t rx_flags:1;
+    uint8_t padding:3;
+    uint8_t channel_plus:1;
+    uint8_t mcs_info:1;
+    uint8_t a_mpdu_stat:1;
+    uint8_t vht_info:1;
+    uint8_t frame_timestamp:1;
+    uint8_t he_info:1;
+    uint8_t he_mu_info:1;
+    uint8_t padding2:1;
+    uint8_t zero_len_psdu:1;
+    uint8_t l_sig:1;
+    uint8_t reserved:1;
+    uint8_t radiotap_ns_next:1;
+    uint8_t vendor_ns_next:1;
+    uint8_t ext:1;
+}Present_flags;
+
+typedef struct Flags{
+    uint8_t cfp:1;
+    uint8_t preamble:1;
+    uint8_t wep:1;
+    uint8_t fragmentation:1;
+    uint8_t fcs_at_end:1;
+    uint8_t data_pad:1;
+    uint8_t bad_fcs:1;
+    uint8_t short_gi:1;
+}Flags;
+
+typedef struct Channel_flags{
+    uint8_t padding:4;
+    uint8_t turbo:1;
+    uint8_t cck:1;
+    uint8_t ofdm:1;
+    uint8_t two_ghz:1;
+    uint8_t five_ghz:1;
+    uint8_t passive:1;
+    uint8_t dynamic_cck_ofdm:1;
+    uint8_t gaussian_freq_shift_keying:1;
+    uint8_t gsm:1;
+    uint8_t static_turbo:1;
+    uint8_t half_rate_chan:1;
+    uint8_t quater_rate_chan:1;
+}Channel_flags;
+
+typedef struct radiotap_header{
+    uint8_t h_revision;
+    uint8_t h_pad;
+    uint16_t h_len;
+    Present_flags presnt_flags;
+}radiotap_header;
+
+typedef struct Dot11_Frame_Control_Field{
+    uint8_t Version:2;
+    uint8_t Type:2;
+    uint8_t Subtype:4;
+    uint8_t Flags;
+}Dot11_Frame_Control_Field;
+
+typedef struct Dot11{
+    Dot11_Frame_Control_Field Frame_Control_Field;
+    uint16_t duration;
+    uint8_t mac1[6];
+    uint8_t mac2[6];
+    uint8_t mac3[6];
+    uint16_t number;
+}Dot11;
+
+typedef struct Dot11_data{
+    Dot11_Frame_Control_Field Frame_Control_Field;
+    uint16_t duration;
+    uint8_t mac1[6];
+    uint8_t mac2[6];
+}Dot11_data;
+
+typedef struct MAC{
+    uint8_t mac[6];
+    bool operator <(const MAC& var) const
+    {
+        return memcmp(mac, var.mac, sizeof(mac)) < 0;
+    }
+}MAC;
+
+typedef struct CONV_MAC{
+    uint8_t bssid[6];
+    uint8_t station[6];
+    bool operator <(const CONV_MAC& var) const
+    {
+        if(memcmp(bssid, var.bssid, sizeof(bssid)) != 0){
+            return memcmp(bssid, var.bssid, sizeof(bssid)) < 0;
+        }else{
+            return memcmp(station, var.station, sizeof(station)) < 0;
+        }
+    }
+}CONV_MAC;
+
+typedef struct Beacon_values{
+    uint8_t PWR;
+    uint16_t CH;
+    int Beacons = 1;
+    char ssid[30];
+
+    bool operator <(const Beacon_values& var) const
+    {
+        if(PWR != var.PWR){
+            return PWR < var.PWR;
+        }else if(CH != var.CH){
+            return CH < var.CH;
+        }else if(Beacons != var.Beacons){
+            return Beacons < var.Beacons;
+        }else{
+            return memcmp(ssid, var.ssid, sizeof(ssid)) < 0;
+        }
+    }
+}Beacon_values;
+
+typedef struct Probe_values{
+    uint8_t PWR;
+    int Frames = 1;
+    uint8_t probe[30];
+
+    bool operator <(const Probe_values& var) const
+    {
+        if(PWR != var.PWR){
+            return PWR < var.PWR;
+        }else if(Frames != var.Frames){
+            return Frames < var.Frames;
+        }else{
+            return memcmp(probe, var.probe, sizeof(probe)) < 0;
+        }
+    }
+}Probe_values;
+
+bool compare(Beacon_values& a, Beacon_values& b){
+    return a < b;
+}
+bool compare(Probe_values& a, Probe_values& b){
+    return a < b;
+}
+
+#pragma pack(pop)
+
+#endif // TPACKETDATA_H
